9092be65e4cf530b3fba982e8e9691def8387447
Yee-Ning Cheng <yee.ningc@gmail.com>
2012-11-05 00:05:21 -0500

    Index shows no results now when a search hasn't been entered

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,7 +2,7 @@ class Actor < ActiveRecord::Base
 	attr_accessible :name, :dateborn, :image_path
 
 	def self.search(search)
-		if search
+		if search && !search.blank?
 			find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
 		else
 			[]
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Wikishow::Application.routes.draw do
   resources :actors, only: [:index, :show]
-  
+
+  root :to => 'actors#index'  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
